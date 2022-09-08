4aac2e6dc306bf7070c253c170911ba27c01e084
PetrikDiak <petrikdiak@gmail.com>
2022-09-08 12:45:00 +0200

    3.a feladat

--- feladat3.js
+++ feladat3.js
@@ -1,5 +1,17 @@
+/*
 function hurra()
 {
     console.log("Hurrá!");
 }
 setInterval(hurra,2000);
+*/
+let szam = 0;
+function szamol()
+{
+    
+    console.log(szam);
+    szam++;
+    
+}
+
+setInterval(szamol,2000);
